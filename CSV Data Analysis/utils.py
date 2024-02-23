20f1f0e71c1995b8935bb91a9bcabc5364a24efb
Atharvabillore01 <91140562+Atharvabillore01@users.noreply.github.com>
2024-02-23 12:39:47 +0530

    Update utils.py

--- CSV Data Analysis/utils.py
+++ CSV Data Analysis/utils.py
@@ -1,8 +1,7 @@
-##from langchain.agents import create_pandas_dataframe_agent #This import has been recently replaced by the below
+
 from langchain_experimental.agents import create_pandas_dataframe_agent
 import pandas as pd
-#from langchain.llms import OpenAI
-#New import from langchain, which replaces the above
+
 from langchain_openai import OpenAI
 
 def query_agent(data, query):
@@ -15,6 +14,4 @@ def query_agent(data, query):
     # Create a Pandas DataFrame agent.
     agent = create_pandas_dataframe_agent(llm, df, verbose=True)
 
-    #Python REPL: A Python shell used to evaluating and executing Python commands. 
-    #It takes python code as input and outputs the result. The input python code can be generated from another tool in the LangChain
     return agent.invoke(query)
